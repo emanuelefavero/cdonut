1a32ab6b1c4f5ee9a524ee1c4d5c08d32d929cd9
emanuelefavero <emafaux89@gmail.com>
2024-10-15 06:26:12 +0200

    add sha256 in Formula/cdonut.rb

--- Formula/cdonut.rb
+++ Formula/cdonut.rb
@@ -2,7 +2,7 @@ class Cdonut < Formula
   desc "A spinning 3D donut in your terminal"
   homepage "https://github.com/emanuelefavero/cdonut"
   url "https://github.com/emanuelefavero/cdonut/archive/refs/tags/v1.0.tar.gz"
-  sha256 "YOUR_TAR_GZ_SHA256_HASH" # Generate this with `shasum -a 256 cdonut-1.0.tar.gz`
+  sha256 "9b558fe41e2bc2776f8e1eb5546072411f0a3135a9a4e35aa1214c8a7d4b71ce" # Generate this with `shasum -a 256 cdonut-1.0.tar.gz`
   version "1.0"
 
   def install
